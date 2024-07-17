d726dcb43a92d5b236b533e6fbcb01ed803de99d
causyj <jainayj804998@gmail.com>
2024-07-17 18:44:31 +0900

    Update tailwind.config.ts : for global style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="2xl">
+    <div>
+      안녕하세요
+      <div className="text-red-800 font-sfpro">
       않은 안녕하세요 AI 닮 가이드 까 출력 생성
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      fontFamily: {
+        cafe24: ['var(--font-cafe24)'],
+        sfpro: ['var(--font-sfpro)'],
+    },
+    fontSize: {
+        '8xl': '6rem',
+        '6xl': '3.75rem',
+        '5xl': '3rem',
+        '3xl': '1.875rem',
+        '2xl': '1.5rem',
+       ' xl': '1.25rem',
+    },
     },
   },
   plugins: [],
