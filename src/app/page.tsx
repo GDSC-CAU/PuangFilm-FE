61720a5fc0d21214ac08357a1685d12cca7b3b21
causyj <jainayj804998@gmail.com>
2024-07-17 18:56:30 +0900

    Update page.tsx: example of font style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      안녕하세요
-      <div className="text-red-800 font-sfpro">
-      않은 안녕하세요 AI 닮 가이드 까 출력 생성
+      기본 글꼴은 cafe24 
+      <div className=" font-sfpro">
+      이 글꼴은 sfpro
       </div>
+      <div className="font-sfpro font-bold">
+        sfpro 글꼴 weight
+      </div>
+      <div className="text-blue-700">
+        글꼴 색
+      </div>
+      
     </div>
   );
 }
