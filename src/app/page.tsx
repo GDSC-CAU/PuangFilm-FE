2727f89600c16a115d5ad65bce08296dd9baf629
causyj <jainayj804998@gmail.com>
2024-07-19 16:58:22 +0900

    Added prettier-plugin-tw : for class automatic alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div>
       기본 글꼴은 cafe24
-      <div className=" font-sfpro">이 글꼴은 sfpro</div>
-      <div className="text-2xl font-sfpro font-bold">sfpro 글꼴 weight</div>
+      <div className="font-sfpro">이 글꼴은 sfpro</div>
+      <div className="font-sfpro text-2xl font-bold">sfpro 글꼴 weight</div>
       <div className="text-blue-700">글꼴 색</div>
     </div>
   );
