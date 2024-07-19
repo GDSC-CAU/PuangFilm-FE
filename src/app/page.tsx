098523bc1e9d88932516e70f5841ac45fa15fb93
causyj <jainayj804998@gmail.com>
2024-07-19 22:35:26 +0900

    Update page.tsx : husky/lint-staged functionality check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      기본 글꼴은 cafe24
+      기본 글꼴은 cafe24d
       <div className="font-sfpro">이 글꼴은 sfpro</div>
       <div className="font-sfpro text-2xl font-bold">sfpro 글꼴 weight</div>
       <div className="text-blue-700">글꼴 색</div>
