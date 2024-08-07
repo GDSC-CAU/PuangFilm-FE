d78c7206c1ec14fe9ed9e3c409511bea2e08e524
falconlee236 <falconlee236@cau.ac.kr>
2024-08-07 20:00:33 +0900

    add frame page routing

---
+++ src/app/frame/page.tsx
@@ -0,0 +1,10 @@
+import MyButton from '@/components/MyButton';
+
+export default function FrameSelectView() {
+  return (
+    <div>
+      <h1>This is 프레임 선택 Page</h1>
+      <MyButton name="다운로드" target="/" />
+    </div>
+  );
+}
