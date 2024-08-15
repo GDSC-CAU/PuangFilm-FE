3d770ddf5d5289a7db1d6cdac14fba6dea2ba19d
causyj <jainayj804998@gmail.com>
2024-08-15 22:00:30 +0900

    Added icons : add previous.svg

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,9 +1,15 @@
 import MyButton from '@/components/MyButton';
+import SVGPrevious from '@/styles/icons/previous.svg';
 
 export default function GuideView() {
   return (
     <div>
+      <div className="absolute left-5 top-5">
+        <SVGPrevious />
+      </div>
+
       <h1>This is guide Page</h1>
+
       <MyButton name="사진 등록하러가기" target="/upload" />
     </div>
   );
