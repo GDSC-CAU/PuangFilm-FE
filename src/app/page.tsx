faed115645519ceedb7459f4df84447fb663baea
falconlee236 <falconlee236@cau.ac.kr>
2024-08-15 23:11:34 +0900

    add image and text with style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,37 @@
 import Image from 'next/image';
 import MyButton from '@/components/MyButton';
+import login_character from '../../public/login_page_img.png';
 
 export default function LoginView() {
   return (
     <div className="flex h-640 w-360 flex-col items-center justify-evenly bg-background">
-      기본 글꼴은 cafe24 this is test
-      <div className="font-sfpro">이 글꼴은 sfpro</div>
-      <div className="font-sfpro text-2xl font-bold">sfpro 글꼴 weight</div>
-      <div className="text-blue-700">글꼴 색</div>
-      <div>This is Login Page</div>
-      <MyButton target="/concept" name="카카오로 로그인하기" />
-      <MyButton target="/list" name="이미 로그인 한 경우" />
+      <div className="text-2xl text-theme-font">
+        어서오세요!
+        <br />
+        푸앙이 사진관입니다 :)
+      </div>
+      <div className="font-sfpro font-bold text-white">
+        AI 프로필을 찍는 푸앙이의 특별한 사진관!
+      </div>
+      <div className="font-sfpro text-white">
+        당신의 자연스러운 모습을
+        <br />
+        푸앙이 프레임에 함께 담아가세요
+      </div>
+      {/* <MyButton target="/list" name="이미 로그인 한 경우" /> */}
       <Image
-        src="/login_page_img.png"
+        src={login_character}
         alt="puang login"
-        width="100"
-        height="100"
+        sizes="100vw"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <MyButton
+        target="/concept"
+        name="카카오로 로그인하기"
+        className="bg-background"
       />
     </div>
   );
