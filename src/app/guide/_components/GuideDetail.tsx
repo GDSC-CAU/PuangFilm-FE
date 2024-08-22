3880ea2f30ebbcea3f6c66a42bc49ac5ccdbe41d
causyj <jainayj804998@gmail.com>
2024-08-22 20:49:41 +0900

    d

--- src/app/guide/_components/GuideDetail.tsx
+++ src/app/guide/_components/GuideDetail.tsx
@@ -23,7 +23,7 @@ function ImageWithDescription({ src, description }: ImageWithDescriptionProps) {
         )}
         {imgError ? (
           <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
-            <span>Image load error</span>
+            <span>Error loading image</span>
           </div>
         ) : (
           <Image
