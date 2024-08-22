66aaad387384512bfc1f661ecb47731cf0e28fa8
causyj <jainayj804998@gmail.com>
2024-08-22 20:52:46 +0900

    Updated GuideDetail.tsx: remove error/loading delay case (will add if needed later)

--- src/app/guide/_components/GuideDetail.tsx
+++ src/app/guide/_components/GuideDetail.tsx
@@ -1,8 +1,4 @@
-// import Image from 'next/image';
-// import SVGTriangle from '@/styles/icons/triangle.svg';
-
 import Image from 'next/image';
-import { useState } from 'react';
 import SVGTriangle from '@/styles/icons/triangle.svg';
 
 interface ImageWithDescriptionProps {
@@ -10,33 +6,17 @@ interface ImageWithDescriptionProps {
   description: string;
 }
 function ImageWithDescription({ src, description }: ImageWithDescriptionProps) {
-  const [imgError, setImgError] = useState(false);
-  const [imgLoaded, setImgLoaded] = useState(false);
-
   return (
     <div className="flex w-full flex-col items-center">
       <div className="relative w-full" style={{ paddingTop: '100%' }}>
-        {!imgLoaded && !imgError && (
-          <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
-            <span>Loading...</span>
-          </div>
-        )}
-        {imgError ? (
-          <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
-            <span>Error loading image</span>
-          </div>
-        ) : (
-          <Image
-            src={src}
-            alt={description}
-            fill
-            style={{ objectFit: 'cover' }}
-            sizes="(max-width: 390px) 100vw"
-            priority
-            onLoad={() => setImgLoaded(true)}
-            onError={() => setImgError(true)}
-          />
-        )}
+        <Image
+          src={src}
+          alt={description}
+          fill
+          style={{ objectFit: 'cover' }}
+          sizes="(max-width: 390px) 100vw"
+          priority
+        />
       </div>
 
       <div className="font-primary-darkblue flex flex-row items-center gap-1 pt-2 font-sfpro text-xs font-extrabold">
