766c7fa426f83f57b183936fc6a38707a5cad181
causyj <jainayj804998@gmail.com>
2024-08-22 20:44:46 +0900

    Updated GuideDetail.tsx: replace example picture& add erro case of image loading/error

--- src/app/guide/_components/GuideDetail.tsx
+++ src/app/guide/_components/GuideDetail.tsx
@@ -1,4 +1,8 @@
+// import Image from 'next/image';
+// import SVGTriangle from '@/styles/icons/triangle.svg';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import SVGTriangle from '@/styles/icons/triangle.svg';
 
 interface ImageWithDescriptionProps {
@@ -6,17 +10,33 @@ interface ImageWithDescriptionProps {
   description: string;
 }
 function ImageWithDescription({ src, description }: ImageWithDescriptionProps) {
+  const [imgError, setImgError] = useState(false);
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
     <div className="flex w-full flex-col items-center">
       <div className="relative w-full" style={{ paddingTop: '100%' }}>
-        <Image
-          src={src}
-          alt={description}
-          fill
-          style={{ objectFit: 'cover' }}
-          sizes="(max-width: 390px) 100vw"
-          priority
-        />
+        {!imgLoaded && !imgError && (
+          <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
+            <span>Loading...</span>
+          </div>
+        )}
+        {imgError ? (
+          <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
+            <span>Image load error</span>
+          </div>
+        ) : (
+          <Image
+            src={src}
+            alt={description}
+            fill
+            style={{ objectFit: 'cover' }}
+            sizes="(max-width: 390px) 100vw"
+            priority
+            onLoad={() => setImgLoaded(true)}
+            onError={() => setImgError(true)}
+          />
+        )}
       </div>
 
       <div className="font-primary-darkblue flex flex-row items-center gap-1 pt-2 font-sfpro text-xs font-extrabold">
@@ -52,7 +72,7 @@ export default function GuideDetail({
         </div>
       </div>
 
-      <div className="grid grid-cols-2 justify-center gap-x-4 gap-y-4 pt-4">
+      <div className="grid grid-cols-2 justify-center gap-x-3 gap-y-4 pt-4">
         {examples.map((example) => (
           <ImageWithDescription
             key={`guide-detail-${example.id}`}
