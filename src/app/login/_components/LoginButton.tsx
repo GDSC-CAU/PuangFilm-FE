61456be43f0c3315c230c6c7c625218d93e5c32c
falconlee236 <falconlee236@cau.ac.kr>
2024-08-24 15:48:56 +0900

    finish create kakao login button

--- src/app/login/_components/LoginButton.tsx
+++ src/app/login/_components/LoginButton.tsx
@@ -1,12 +1,19 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import LoginImage from '@/styles/images/Kakao-Login.png';
 
 export default function LoginButton() {
   return (
-    <Link
-      href="/concept"
-      className="flex h-12 w-full items-center justify-center rounded-b bg-yellow-400 text-xl text-black"
-    >
-      카카오로 로그인 하기
+    <Link href="/concept">
+      <Image
+        src={LoginImage}
+        alt="kakao login"
+        sizes="100vw"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+      />
     </Link>
   );
 }
