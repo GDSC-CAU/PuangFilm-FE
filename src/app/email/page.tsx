41646092000571e23b31b0f9caf2076a4ab1b656
causyj <jainayj804998@gmail.com>
2024-08-25 14:46:52 +0900

    Updated pages.tsx: add previous btn & title

--- src/app/email/page.tsx
+++ src/app/email/page.tsx
@@ -1,9 +1,15 @@
 import MyButton from '@/components/MyButton';
+import PreviousPage from '@/components/PreviousPage';
 
 export default function EmailEnterView() {
   return (
-    <div>
-      <h1>This is Email Enter Page</h1>
+    <div className="w-full">
+      <PreviousPage target="/upload" />
+      <div className="text-center text-xl">
+        프로필을 생성하는데 <br />
+        다소 시간이 걸릴 수 있어요!
+      </div>
+
       <MyButton name="확인" target="/waiting" />
     </div>
   );
