2b88e21d74cab1bd20fe7137fb975297328d6c84
falconlee236 <falconlee236@cau.ac.kr>
2024-08-27 17:14:09 +0900

    unresolved for cors policy

--- src/app/login/_components/LoginButton.tsx
+++ src/app/login/_components/LoginButton.tsx
@@ -6,17 +6,20 @@ import LoginImage from '@/styles/images/Kakao-Login.png';
 
 export default function LoginButton() {
   // TODO - handle cors policy
-  const URL =
+  const REDIRECT_URL =
     `https://kauth.kakao.com/oauth/authorize` +
-    `?client_id=${process.env.LOGIN_API_KEY}&` +
-    `redirect_uri=${process.env.LOGIN_REDIRECT_URL}&response_type=code&scope=openid`;
+    `?client_id=${process.env.NEXT_PUBLIC_LOGIN_API_KEY}&` +
+    `redirect_uri=${process.env.NEXT_PUBLIC_LOGIN_REDIRECT_URL}&response_type=code&scope=openid`;
   const loginProcess = async () => {
-    const res = await fetch(URL);
-    if (!res.ok) {
-      throw new Error();
-    }
-    console.dir(res.json());
+    window.location.href = REDIRECT_URL;
+
+    const AUTHORIZATION_CODE = new URL(
+      document.location.toString(),
+    ).searchParams.get('code') as string;
+
+    console.log(AUTHORIZATION_CODE);
   };
+
   return (
     <Link href="/concept">
       <button className="h-full w-full" type="button" onClick={loginProcess}>
