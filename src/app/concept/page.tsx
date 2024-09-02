bcaabe1186eb967a16d40fb5a97450e3b6d653cc
hoon <ldh019@naver.com>
2024-09-02 15:27:59 +0900

    Update concept/page.tsx: Add gender state

--- src/app/concept/page.tsx
+++ src/app/concept/page.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import { atom, useAtom } from 'jotai';
+import Image from 'next/image';
 import MyButton from '@/components/MyButton';
 
+const selectedBoxAtom = atom<'female' | 'male' | null>(null);
+
 export default function SelectConceptView() {
+  const [selectedBox, setSelectedBox] = useAtom(selectedBoxAtom);
+
+  const handleBoxClick = (box: 'female' | 'male') => {
+    if (selectedBox === box) {
+      setSelectedBox(null);
+    } else {
+      setSelectedBox(box);
+    }
+  };
+
   return (
-    <div className="mx-10 flex w-full flex-col gap-10">
-      <p className="text-#133365 text-start font-cafe24 text-xl">
+    <div className="relative mx-5 flex w-full flex-col gap-10">
+      <p className="mt-10 text-start font-cafe24 text-xl text-primary-darkblue">
         어떤 컨셉의 <br />
         프로필을 만들어볼까요?
       </p>
@@ -11,16 +27,55 @@ export default function SelectConceptView() {
         원하는 느낌의 <b>프로필 컨셉</b>을 골라주세요!
       </p>
       <div className="flex w-full flex-row items-center justify-center gap-10">
-        <div className="gap-10">
-          {/* add image */}
-          <p>여자</p>
-        </div>
-        <div className="">
-          {/* add image */}
-          <p>남자</p>
+        <div className="grid w-full grid-cols-2 justify-center gap-5">
+          <div className="justify-center">
+            <button
+              className="w-full cursor-pointer"
+              onClick={() => handleBoxClick('female')}
+              type="button"
+              aria-label="female"
+              style={{
+                boxSizing: 'border-box',
+                border:
+                  selectedBox === 'female'
+                    ? '4px solid #133365'
+                    : '4px solid transparent',
+              }}
+            >
+              <div className="h-48 w-full bg-white" />
+            </button>
+            <p className="mt-4 text-center">여자</p>
+          </div>
+          <div className="justify-center">
+            <button
+              className="w-full cursor-pointer"
+              onClick={() => handleBoxClick('male')}
+              type="button"
+              aria-label="male"
+              style={{
+                boxSizing: 'border-box',
+                border:
+                  selectedBox === 'male'
+                    ? '4px solid #133365'
+                    : '4px solid transparent',
+              }}
+            >
+              <div className="h-48 w-full bg-white" />
+            </button>
+            <p className="mt-4 text-center">남자</p>
+          </div>
         </div>
       </div>
-      <MyButton name="다음" target="/guide" />
+      <MyButton name="다 음" target="/guide" enabled={selectedBox != null} />
+
+      <div className="absolute right-0 top-0 h-40 w-40">
+        <Image
+          src="/corner-puang.png"
+          alt="이미지 설명"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
     </div>
   );
 }
