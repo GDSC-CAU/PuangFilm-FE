44723b197af4686be3a0acaad63fb5da067d4e68
falconlee236 <falconlee236@cau.ac.kr>
2024-09-02 16:18:47 +0900

    Update route.ts : create server route api to detour cors Error

---
+++ src/app/api/reissue/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url);
+  const searchCode = searchParams.get('code') || '';
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}:${process.env.NEXT_PUBLIC_SERVER_PORT}/api/auth/reissue`,
+    {
+      headers: {
+        Authorization: `Bearer ${searchCode}`,
+      },
+    },
+  );
+  const jsonData = await response.json();
+
+  return NextResponse.json(
+    {
+      accessToken: jsonData.data?.accessToken,
+      message: jsonData.msg,
+    },
+    {
+      status: jsonData.code,
+    },
+  );
+};
