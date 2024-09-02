b763d2094de2ba6fca1162fcb8c991e13b4df91f
falconlee236 <falconlee236@cau.ac.kr>
2024-09-02 16:17:14 +0900

    Update list/page.tsx, concept/page.tsx : add console.log debug to check how to use getting token with jotai useAtomValue API for other user

--- src/app/concept/page.tsx
+++ src/app/concept/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
+import { useAtomValue } from 'jotai';
 import MyButton from '@/components/NextButton';
+import { tokenAtom } from '@/store/atoms/tokenAtom';
 
 export default function SelectConceptView() {
+  const token = useAtomValue(tokenAtom);
+  console.log(token);
+
   return (
     <div>
       <h1>This is Concept Page</h1>
