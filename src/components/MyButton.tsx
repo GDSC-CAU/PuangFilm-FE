ece45bc4cf6f2c8aa01604690a4528b3576fdc82
hoon <ldh019@naver.com>
2024-09-02 15:28:52 +0900

    Update MyButton.tsx: Add unabled design

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -3,13 +3,18 @@ import Link from 'next/link';
 interface MyButtonProps {
   name: string;
   target: string;
+  enabled: boolean;
 }
 
-export default function MyButton({ name, target }: MyButtonProps) {
+export default function MyButton({
+  name,
+  target,
+  enabled = true,
+}: MyButtonProps) {
   return (
     <Link
-      href={target}
-      className="flex h-12 w-full items-center justify-center rounded-full bg-primary-darkblue text-xl text-white"
+      href={enabled ? target : '#'}
+      className={`flex h-12 w-full items-center justify-center rounded-full text-xl ${enabled ? 'bg-primary-darkblue text-white' : 'text-primary-darkgray cursor-not-allowed bg-primary-gray'}`}
     >
       {name}
     </Link>
