a60160b9ddb07c6ce54f34e4169aad326655341f
falconlee236 <falconlee236@cau.ac.kr>
2024-09-18 14:40:17 +0900

    Update IconButton.tsx  : remove unnecessary constant in component file

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,32 +1,21 @@
 import Link from 'next/link';
-import { ICON_TYPES, IconButtonProps, IconProps, IconType } from '@/interfaces';
-import SVGImages from '@/styles/icons/images.svg';
-import SVGNext from '@/styles/icons/next.svg';
-
-const icons: Record<IconType, IconProps> = {
-  [ICON_TYPES.NEXT]: {
-    component: <SVGNext />,
-    bgColor: 'bg-primary-darkblue',
-  },
-  [ICON_TYPES.IMAGES]: {
-    component: <SVGImages />,
-    bgColor: 'bg-primary-comet',
-  },
-};
+import { iconList } from '@/constants';
+import { ICON_TYPES, IconButtonProps } from '@/interfaces';
 
 export default function IconButton({
   name,
   target,
   iconType,
+  iconComponent,
 }: IconButtonProps) {
   return (
     <Link
       href={target}
-      className={`flex h-12 w-full items-center justify-evenly rounded-full text-xl ${iconType && icons[iconType].bgColor} text-white`}
+      className={`flex h-12 w-full items-center justify-evenly rounded-full text-xl ${iconList[iconType].bgColor} text-white`}
     >
-      {iconType && iconType === ICON_TYPES.IMAGES && icons[iconType].component}
+      {iconType === ICON_TYPES.IMAGES && iconComponent}
       {name}
-      {iconType && iconType === ICON_TYPES.NEXT && icons[iconType].component}
+      {iconType === ICON_TYPES.NEXT && iconComponent}
     </Link>
   );
 }
