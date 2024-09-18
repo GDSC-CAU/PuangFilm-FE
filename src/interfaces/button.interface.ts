ddb5542b43009038d710c0ee4a05e04b67f58612
falconlee236 <falconlee236@cau.ac.kr>
2024-09-18 13:50:39 +0900

    Update interfaces/button.interface.ts  : add Button icon interface

--- src/interfaces/button.interface.ts
+++ src/interfaces/button.interface.ts
@@ -1,5 +1,20 @@
-export interface MyButtonProps {
+import { ReactElement } from 'react';
+import { IconType } from '@/interfaces/icon.interfaces';
+
+interface ButtonProps {
   name: string;
   target: string;
+}
+
+export interface MyButtonProps extends ButtonProps {
   enabled?: boolean;
+}
+
+export interface IconButtonProps extends ButtonProps {
+  iconType?: IconType;
+}
+
+export interface IconProps {
+  component: ReactElement;
+  bgColor: string;
 }
