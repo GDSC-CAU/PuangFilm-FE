d515c76e0b14404c7b2a39740f25997d742e395e
falconlee236 <falconlee236@cau.ac.kr>
2024-09-20 11:21:39 +0900

    Update tailwind.config.ts  : modify content path string

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   mode: 'jit',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/constants/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
