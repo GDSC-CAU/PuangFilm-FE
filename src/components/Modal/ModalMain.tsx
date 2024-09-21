1232782413f0fa7ac4be7117456b3b0d3abe223f
causyj <jainayj804998@gmail.com>
2024-09-21 12:23:38 +0900

    Update MOdalMain.tsx: remove unnecessary part

--- src/components/Modal/ModalMain.tsx
+++ src/components/Modal/ModalMain.tsx
@@ -22,7 +22,7 @@ function ModalMain({
 
   return createPortal(
     <div
-      className={`fixed inset-0 z-40 flex ${modalLocation} justify-center bg-black bg-opacity-50`}
+      className={`fixed inset-0 flex ${modalLocation} justify-center bg-black bg-opacity-50`}
     >
       <div className="w-full max-w-[420px] px-6">
         <div className="flex w-full flex-col items-center justify-evenly gap-y-4 rounded-[20px] bg-white px-6 pt-6">
