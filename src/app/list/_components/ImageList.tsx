b07d40e078e81bb62fa23334676a617dcee44840
hoon <ldh019@naver.com>
2024-10-01 18:12:59 +0900

    Update list/ImageList.tsx: Add all components without image loading

--- src/app/list/_components/ImageList.tsx
+++ src/app/list/_components/ImageList.tsx
@@ -1,22 +1,67 @@
+'use client';
+
+import MyButton from "@/components/MyButton";
+import { ROUTE_TYPES } from "@/interfaces";
+import { gender } from "@/store/atoms/selectedBoxAtom";
+import { useAtom } from 'jotai/index';
+import Image from 'next/image';
+import { selectedPhotoAtom } from '@/store/atoms/selectedPhotoAtom';
+
 interface ListWithDataProps {
-  list: { date: string; index: number }[];
+  list: string[];
 }
 
-export default function ImageList({ list: [] }): JSX.Element {
+export default function ImageList({ list }: ListWithDataProps): JSX.Element {
+  const [selectedPhoto, setSelectedPhoto] = useAtom(selectedPhotoAtom);
+
+  const handlePhotoClick = (box: string) => {
+    if (selectedPhoto === box) {
+      setSelectedPhoto('');
+    } else {
+      setSelectedPhoto(box);
+    }
+  };
+
   return (
     <div>
-      <p className="text-center font-sfpro text-sm text-white">
+      <p className="mb-5 text-center font-sfpro text-sm text-white">
         편집할 사진을 선택해서 <br />
         이미지 프레임을 선택해 주세요!
       </p>
 
-      <div className="h-64 overflow-y-auto">
+      <div className="h-96 overflow-y-auto">
         <div className="grid grid-cols-2 gap-4">
-          {list.map((date: string, index: number) => (
-            <div key={date} className="rounded-md bg-gray-100 p-4 shadow-md" />
+          {list.map((item) => (
+            <div>
+              <button
+                key={item}
+                className="w-full cursor-pointer"
+                onClick={() => handlePhotoClick(item)}
+                type="button"
+                aria-label="female"
+                style={{
+                  boxSizing: 'border-box',
+                  border:
+                    selectedPhoto === item
+                      ? '4px solid #133365'
+                      : '4px solid transparent',
+                }}
+              >
+                <Image
+                  src="/curious-puang.png"
+                  alt=""
+                  width={200}
+                  height={200}
+                />
+              </button>
+              <p className="text-right font-sfpro text-sm text-white">
+                {item}
+              </p>
+            </div>
           ))}
         </div>
       </div>
+      <MyButton name="확 인" target={ROUTE_TYPES.FRAME} />
     </div>
   );
 }
