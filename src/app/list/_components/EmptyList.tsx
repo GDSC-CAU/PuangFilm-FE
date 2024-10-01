63d7c2bc4adf2d1b91157adc7fcf903b63a7a921
hoon <ldh019@naver.com>
2024-10-01 16:41:50 +0900

    Added EmptyList.tsx

---
+++ src/app/list/_components/EmptyList.tsx
@@ -0,0 +1,10 @@
+export default function EmptyList() {
+  return (
+    <div>
+      <p className="text-center font-sfpro text-sm text-primary-darkblue">
+        아직 생성된 프로필이 없어요. <br />
+        프로필을 먼저 만들어 주세요!
+      </p>
+    </div>
+  );
+}
