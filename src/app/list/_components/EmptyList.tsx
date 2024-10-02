91e8ef8314b987298bd1637857b789058b349267
hoon <ldh019@naver.com>
2024-10-02 14:43:21 +0900

    Update list/EmptyList: Edit Button size & Image position

--- src/app/list/_components/EmptyList.tsx
+++ src/app/list/_components/EmptyList.tsx
@@ -5,13 +5,13 @@ import SVGNext from '@/styles/icons/next.svg';
 
 export default function EmptyList() {
   return (
-    <div className="mx-auto max-w-md text-center">
+    <div className="mx-auto w-full text-center">
       <p className="mb-10 text-center font-sfpro text-sm text-primary-darkblue">
         아직 생성된 프로필이 없어요. <br />
         프로필을 먼저 만들어 주세요!
       </p>
       <Image
-        className="mb-10"
+        className="mx-auto mb-10"
         src="/curious-puang.png"
         alt="어리둥절한 푸앙이"
         width={270}
