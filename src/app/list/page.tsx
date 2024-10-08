d6c48cbce64e59a068683589d5b17369cdb83011
hoon <ldh019@naver.com>
2024-10-08 13:19:13 +0900

    Update list/page.tsx: Add list API call to get profile list

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,21 +1,34 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import PreviousPage from '@/components/PreviousPage';
 import EmptyList from './_components/EmptyList';
 import ImageList from './_components/ImageList';
 
 export default function ListView() {
-  const list = [
-    'wowowow',
-    'wowowowowo',
-    'wowowowowowowo',
-    'wowowow',
-    'wowowowowo',
-    'wowowowowowowo',
-    'wowowow',
-    'wowowowowo',
-    'wowowowowowowo',
-  ];
+  const [list, setList] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/photo-request/list');
+        const data = await response.json();
+
+        if (data.code === 200) {
+          setList(data.data);
+        } else {
+          console.error(data.msg);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData().then(() => {});
+  }, []);
 
   return (
     <div className="flex w-full flex-col justify-start bg-background">
@@ -23,7 +36,8 @@ export default function ListView() {
       <p className="mb-3 text-center font-cafe24 text-xl text-primary-darkblue">
         나의 프로필 목록
       </p>
-      {list.length === 0 ? <EmptyList /> : <ImageList list={list} />}
+      {list.length === 0 && <EmptyList />}
+      {!loading && list.length !== 0 && <ImageList list={list} />}
     </div>
   );
 }
