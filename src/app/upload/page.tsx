2645426484778d1ec2ee5f303b322325459cade8
1lsang <ilsangpark.98@gmail.com>
2024-10-13 23:33:30 +0900

    Update upload/page.tsx

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,10 +1,49 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import ImageUpload from '@/app/upload/_components/ImageUpload';
 import MyButton from '@/components/MyButton';
+import PreviousPage from '@/components/PreviousPage';
+import { IMAGE_WARNING, MAX_IMAGE_NUMBER, MIN_IMAGE_NUMBER } from '@/constants';
+import { ImageFile } from '@/interfaces';
 
 export default function PhotoUploadView() {
+  const [images, setImages] = useState<ImageFile[]>([]);
+
   return (
-    <div>
-      <h1>This is Image Upload Page</h1>
-      <MyButton name="다음" target="/email" />
+    <div className="w-full text-primary-darkblue">
+      <div className="mb-5 flex w-full flex-row justify-between px-4">
+        <PreviousPage target="/" />
+      </div>
+      <div className="mb-3.5 flex items-center gap-6">
+        <div>
+          <Image
+            src="/upload-puang.png"
+            alt="푸앙이 프로필"
+            width="72"
+            height="72"
+          />
+        </div>
+        <div className="flex items-end gap-1">
+          <h2 className="whitespace-pre-line text-xl">
+            {'사진은\n'}
+            <span className="bg-white">최소 8장 이상</span>
+            {'\n등록해주세요!'}
+          </h2>
+          <div className="text-3xs text-[#3C628C]">(최대 16장)</div>
+        </div>
+      </div>
+      <ImageUpload
+        images={images}
+        setImages={setImages}
+        min={MIN_IMAGE_NUMBER}
+        max={MAX_IMAGE_NUMBER}
+      />
+      <p className="mb-6 mt-2.5 whitespace-pre-line font-sfpro text-3xs font-bold text-white">
+        {IMAGE_WARNING}
+      </p>
+      <MyButton name="다음" target="/email" enabled={images.length >= 8} />
     </div>
   );
 }
---
+++ src/constants/image.ts
@@ -0,0 +1,5 @@
+export const IMAGE_WARNING =
+  '* 가이드에 부합하지 않는 사진을 업로드 할 경우,\n 나와 닮지 않은 AI이미지가 생성될 수 있어요.';
+
+export const MIN_IMAGE_NUMBER = 8;
+export const MAX_IMAGE_NUMBER = 16;
