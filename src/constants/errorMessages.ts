1c32789ddbc013350dde01c61c6293c0cdbd5ac5
1lsang <ilsangpark.98@gmail.com>
2024-10-14 01:42:49 +0900

    Update errorMessages.ts: Add upload error message

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -3,3 +3,5 @@ export const LOGIN_ERROR_CHECK_MSG: string =
   '아이디와 비밀번호를 다시 확인해주세요';
 export const GENERATION_ERROR_MSG: string = '프로필을 생성하는 실패했습니다.';
 export const GENERATION_ERROR_CHECK_MSG: string = '이미지를 다시 확인해주세요';
+export const UPLOAD_ERROR_MSG: string = '이미지 업로드에 실패했습니다.';
+export const UPLOAD_ERROR_CHECK_MSG: string = '이미지를 다시 확인해주세요';
