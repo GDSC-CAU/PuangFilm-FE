9455ea9e83f1c90fd6161280783f5dba188c3631
causyj <jainayj804998@gmail.com>
2024-10-18 10:19:25 +0900

    page.tsx: edit header

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,18 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = async (request: NextRequest) => {
-  const { searchParams } = new URL(request.url);
-  const searchCode = searchParams.get('code') || '';
+  const { headers } = request;
+
   const body = await request.json();
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}:${process.env.NEXT_PUBLIC_SERVER_PORT}/api/photo-request`,
     {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${searchCode}`,
-      },
+      headers,
       body: JSON.stringify(body),
     },
   );
